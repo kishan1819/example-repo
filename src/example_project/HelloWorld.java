8a9b081638735dde93703b253a729e7d5d1f3f00
kishan1819 <57606726+kishan1819@users.noreply.github.com>
2019-11-10 20:51:47 -0500

    Update HelloWorld.java

--- src/example_project/HelloWorld.java
+++ src/example_project/HelloWorld.java
@@ -23,9 +23,7 @@ public class HelloWorld {
         int num = input.nextInt();
         input.close();
         System.out.println("The square of " + num + " is " + num * num);
-        System.out.println("This is new to the file");
-
-        // This is a new message
+        System.out.println("This is a branch");
     }
 
 }
