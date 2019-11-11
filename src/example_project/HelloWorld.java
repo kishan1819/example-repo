a943435897df79b1205dec18548bcc151f90c44c
kishan1819 <57606726+kishan1819@users.noreply.github.com>
2019-11-10 20:43:18 -0500

    Update HelloWorld.java

--- src/example_project/HelloWorld.java
+++ src/example_project/HelloWorld.java
@@ -17,6 +17,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        Scanner input = new Scanner(System.in);
         System.out.println("Hello world");
         System.out.println("Enter a number: ");
         int num = input.nextInt();
