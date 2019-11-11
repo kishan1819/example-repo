47a05ebf5f3c7ab9e4b2ca09d549b0c6166768f4
unknown <kishan1819@vt.edu>
2019-11-10 20:38:45 -0500

    Created java project

---
+++ src/example_project/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * This is an example java project
+ * in my first GitHub repository
+ */
+package example_project;
+
+/**
+ * @author Kishan
+ *
+ */
+public class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello world");
+    }
+
+}
